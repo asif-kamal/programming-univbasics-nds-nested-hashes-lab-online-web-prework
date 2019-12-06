668814bcd74914c9e8da3c2beafac5680f2103be
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 01:55:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,8 +19,7 @@ def assembled_aoh
 
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  
 end
 
 def aoh_lookup(aoh, row, key)
