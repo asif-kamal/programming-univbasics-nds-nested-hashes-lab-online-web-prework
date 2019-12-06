51e2d8bceb751213c75d01f5825f7e337e846526
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 02:00:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -33,6 +33,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
+  assembled_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
   # Return the updated AoH
 end
