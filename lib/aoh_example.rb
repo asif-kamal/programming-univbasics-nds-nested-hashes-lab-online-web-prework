5153dd8c9fd2bd3124bfaca38cfd9873fd4e6c38
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 01:56:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,6 +20,12 @@ def assembled_aoh
 
 def literal_aoh
   
+  infinite_jest =
+       [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
+       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+       {:name=>"Pat Monteseian", :occupation=>"Staff"},
+       {:name=>"Kate Gompert", :occupation=>"None"},
+       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}] 
 end
 
 def aoh_lookup(aoh, row, key)
