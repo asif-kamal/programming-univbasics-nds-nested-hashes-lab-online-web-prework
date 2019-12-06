ccea617b56daefe6b5c9568b953685f0a350c031
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 01:39:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,6 +13,7 @@ def assembled_aoh
        {:name=>"Pat Monteseian", :occupation=>"Staff"},
        {:name=>"Kate Gompert", :occupation=>"None"},
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}] 
+     end
        
        infinite_jest
 end
